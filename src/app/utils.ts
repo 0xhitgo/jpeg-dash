968409751860ff9b702c56dfefe38b2d007b6800
0xhitgo <0xhitgo@sentiment.xyz>
2023-07-11 23:19:53 +0530

    chore: update env variable name
    Signed-off-by: 0xhitgo <0xhitgo@sentiment.xyz>

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,6 +1,6 @@
 import { createClient, fetchExchange } from "urql";
 
 export const jpegdAnalyticsClient = createClient({
-  url: process.env.NEXT_PONDER_GRAPHQL_URL || "http://localhost:42069",
+  url: process.env.NEXT_PUBLIC_PONDER_GRAPHQL_URL || "http://localhost:42069",
   exchanges: [fetchExchange],
 });
